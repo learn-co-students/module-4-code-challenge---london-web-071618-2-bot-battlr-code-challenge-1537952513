440afaead09d68c9b045e6098a98a057f9181fd9
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-09-26 10:59:07 +0100

    checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,44 @@
 import React from "react";
+import BotCard from '../components/BotCard'
+import YourBotArmy from './YourBotArmy'
+
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state={
+    allBots: [],
+    botArmy: []
+  }
+  
+  componentDidMount = () => {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(resp => resp.json())
+    .then(json => this.setState({allBots: json}))
+  }
+
+  selectedBot = (bot) => {
+    if(!this.state.botArmy.includes(bot)) {
+    this.setState({ botArmy: [...this.state.botArmy, bot] })
+    } else {
+      let newArmy = this.state.botArmy
+      let index = this.state.botArmy.indexOf(bot)
+      if (index > -1) {
+        let newArmy = this.state.botArmy.splice(index, 1)
+      }
+      this.setState({ botArmy: newArmy})
+    }
+  }
+
+  
 
   render() {
+
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botArmy} selectedBot={ this.selectedBot }/>
+        { this.state.allBots.map(bot => 
+          <BotCard bot={bot} selectedBot={ this.selectedBot } />
+          ) }
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,15 +1,18 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
-class YourBotArmy extends React.Component {
-  //your bot army code here...
 
+class YourBotArmy extends React.Component {
+
+  
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            { this.props.botArmy.map(bot => 
+              <BotCard bot={bot} selectedBot={ this.props.selectedBot }/>
+              ) }
             Your Bot Army
           </div>
         </div>
