d4d694c5754bbd383872ae313c5bd4820f423e0d
ritz-w <ritz.wu@outlook.com>
2018-09-26 10:21:31 +0100

    reached mvp

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,32 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  constructor(props) {
+		super(props)
+		this.state = {
+			allBots: [],
+			yourBotArmy: []
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+		.then(res => res.json())
+		.then(botData => this.setState({allBots: botData}))
+  }
+  
+  recruitBot = (bot) => {
+    this.setState({yourBotArmy: [...this.state.yourBotArmy, bot]}, () => console.log(this.state.yourBotArmy))
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy bots={this.state.yourBotArmy} />
+        <BotCollection bots={this.state.allBots} recruitBot={this.recruitBot}/>
       </div>
     );
   }
