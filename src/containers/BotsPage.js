0a4593433301b936bbf93d310ba33a4d8685e6d7
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-09-26 10:56:22 +0100

    Checkpoint Commit Jo

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React from 'react'
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  // start here with your code for step one
 
-  render() {
-    return (
-      <div>
-        {/* put your components here */}
-      </div>
-    );
+  constructor () {
+    super()
+    this.state = {
+      allBots: [],
+      yourBotArmy: []
+    }
   }
 
+  componentDidMount () {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(resp => resp.json())
+      .then(data => this.setState({ allBots: data }))
+  }
+
+  enlistBot = (selectedBot) => {
+    if (!this.state.yourBotArmy.find(bot => bot === selectedBot)) {
+      this.setState({ yourBotArmy: [...this.state.yourBotArmy, selectedBot] })
+    }
+  }
+
+  delistBot = (selectedBot) => {
+    if (this.state.yourBotArmy.find(bot => bot === selectedBot)) {
+      this.setState({ yourBotArmy: this.state.yourBotArmy.filter(bot => bot !== selectedBot) })
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <YourBotArmy yourBotArmy={this.state.yourBotArmy} delistBot={this.delistBot} />
+        <BotCollection allBots={this.state.allBots} enlistBot={this.enlistBot} />
+      </div>
+    )
+  }
 }
 
-export default BotsPage;
+export default BotsPage
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,22 +1,22 @@
-import React from "react";
-import BotCard from "../components/BotCard";
+import React from 'react';
+import BotCard from '../components/BotCard';
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
 
-  render(){
+  render () {
     return (
-      <div className="ui segment inverted olive bot-army">
-        <div className="ui five column grid">
-          <div className="row bot-army-row">
-            {/*...and here...*/}
+      <div className='ui segment inverted olive bot-army'>
+        <div className='ui five column grid'>
+          <div className='row bot-army-row'>
+            {this.props.yourBotArmy.map(bot => {
+              return <BotCard bot={bot} enlistDelistBot={this.props.delistBot} />
+            })}
             Your Bot Army
           </div>
         </div>
       </div>
-    );
+    )
   }
-  
 };
 
-export default YourBotArmy;
+export default YourBotArmy
